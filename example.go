c96d63158d0c469f291873010271ed070bb9f37e
wanghongwei2 <wanghongwei2@100tal.com>
2019-07-06 19:42:54 +0800

    修复release bug 防止close已关闭的chan

--- example.go
+++ example.go
@@ -1,40 +1,116 @@
-package justOnceWhileCocurrent
+package main
 
 import (
 	"fmt"
 	"github.com/abusizhishen/justOnceWhileCocurrent/src"
+	"github.com/pkg/errors"
+	"log"
+	"sync"
 	"time"
 )
 
 func main() {
-	var requestIdentifie = "382h23ehd32je32"
-	for i:=0;i<10;i++{
-		go add(requestIdentifie)
+	for i := 0; i < 10; i++ {
+		//并发do something
+		go doSomeThing()
 	}
 
-	for i:=0;i<5;i++{
-		fmt.Println(n)
-		time.Sleep(time.Second)
-	}
+	time.Sleep(time.Second*5)
 }
 
-var n int
 var once src.JustOnceSameTime
 
-//example for usage
-// i 是
-func add(requestIdentifie interface{})  {
-	if once.Req(requestIdentifie){
-		fmt.Println(requestIdentifie,"获得锁，Go")
-		//为演示效果，sleep
-		time.Sleep(time.Second*3)
-		n++
+func doSomeThing() {
+	var userId = 12345
+	var user, err = getUserInfo(userId)
+	fmt.Println(user, err)
+}
 
-		//得到资源后释放锁
-		once.Release(requestIdentifie)
-	}else {
-		fmt.Println("没抢到锁，等待抢到锁的线程执行结束。。。")
-		once.Wait(requestIdentifie)
-		fmt.Println("等待结束，获取结果为:",n)
+//example for usage
+// 演示获取用户详情的过程，先从本地缓存读取用户,如果本地缓存不存在,就从redis读取
+var keyUser = "user_%d"
+
+func getUserInfo(userId int) (user UserInfo, err error) {
+	user, err = userCache.GetUser(userId)
+	if err == nil {
+		return
 	}
+
+	var requestTag = fmt.Sprintf(keyUser, userId)
+	if !once.Req(requestTag) {
+		fmt.Println("没抢到锁，等待抢到锁的线程执行结束。。。")
+		once.Wait(requestTag)
+		fmt.Println("等待结束:", keyUser)
+		return userCache.GetUser(userId)
+	}
+
+	//得到资源后释放锁
+	defer once.Release(requestTag)
+	fmt.Println(requestTag, "获得锁，let's Go")
+
+	//为演示效果，sleep
+	time.Sleep(time.Second * 3)
+
+	//redis读取用户信息
+	log.Println("redis读取用户信息:", userId)
+	user, err = getUserInfoFromRedis(userId)
+	if err != nil {
+		return
+	}
+
+	//用户写入缓存
+	log.Println("用户写入缓存:",userId)
+	userCache.setUser(user)
+	return
+}
+
+//用户信息缓存
+type UserCache struct {
+	Users map[int]UserInfo
+	sync.RWMutex
+}
+
+type UserInfo struct {
+	Id     int
+	Name   string
+	Age    int
+	Gender int
+	Img    string
+}
+
+var userCache UserCache
+var errUserNotFound = errors.New("user not found in cache")
+
+func (c *UserCache) GetUser(id int) (user UserInfo, err error) {
+	c.RLock()
+	var ok bool
+	user, ok = userCache.Users[id]
+	if ok {
+		return
+	}
+
+	c.RUnlock()
+	return user, errUserNotFound
+}
+
+func (c *UserCache) setUser(user UserInfo) {
+	c.Lock()
+	if c.Users == nil {
+		c.Users = make(map[int]UserInfo)
+	}
+
+	c.Users[user.Id] = user
+	c.Unlock()
+	return
+}
+
+func getUserInfoFromRedis(id int) (user UserInfo, err error) {
+	// ...
+
+	user = UserInfo{
+		Id:   12345,
+		Name: "someone",
+		Age:  18,
+	}
+	return
 }
--- src/once.go
+++ src/once.go
@@ -7,45 +7,54 @@ import (
 
 type JustOnceSameTime struct {
 	Lock sync.RWMutex
-	Map map[interface{}]chan bool
+	Map  map[interface{}]chan bool
 }
 
-func (u *JustOnceSameTime)Req(key interface{}) bool {
+/*
+RequestTag 请求标识 用于标识同一个资源
+同一时刻只有一个请求能获取执行权限，获得执行权限的线程接下来需要执行具体的业务逻辑，
+完成后调用release方法通知其他线程，操作已完成，获取资源即可
+其他请求接下来需要调用wait方法
+*/
+func (u *JustOnceSameTime) Req(RequestTag interface{}) bool {
 	u.Lock.Lock()
 	defer u.Lock.Unlock()
 
-	if u.Map == nil{
+	if u.Map == nil {
 		u.Map = make(map[interface{}]chan bool)
 	}
 
-	_,ok := u.Map[key]
-	if ok{
-		//log.Println("没有得到锁，等待执行者执行结束")
+	_, ok := u.Map[RequestTag]
+	if ok {
+		log.Println("没有得到锁，等待执行者执行结束")
 		return false
 	}
 
-	u.Map[key] = make(chan bool,1)
-	//log.Println("获取锁")
+	u.Map[RequestTag] = make(chan bool, 1)
+	log.Println("获取锁:", RequestTag)
 
 	return true
 }
 
-func (u *JustOnceSameTime)Wait(key interface{}){
-	for{
+/*RequestTag 请求标识 用于标识同一个资源
+调用wait方法将处于阻塞状态，直到获得执行权限的线程处理完具体的业务逻辑，调用release方法来通知其他线程资源ok了
+*/
+func (u *JustOnceSameTime) Wait(RequestTag interface{}) {
+	for {
 		u.Lock.RLock()
-		_,ok := u.Map[key]
+		_, ok := u.Map[RequestTag]
 		if !ok {
-			//log.Println("等待结束：")
+			log.Println("等待结束：", RequestTag)
 			u.Lock.RUnlock()
 			return
 		}
 		select {
-		case _,ok:= <- u.Map[key]:
+		case _, ok := <-u.Map[RequestTag]:
 			if !ok {
-				//log.Println("等待结束：")
+				log.Println("等待结束：", RequestTag)
 				u.Lock.RUnlock()
 				return
-			}else{
+			} else {
 				log.Println(ok)
 			}
 		default:
@@ -54,12 +63,18 @@ func (u *JustOnceSameTime)Wait(key interface{}){
 	}
 }
 
-func (u *JustOnceSameTime)Release(str interface{}){
+/*RequestTag 请求标识 用于标识同一个资源
+获得执行权限的线程需要在执行完业务逻辑后调用该方法通知其他处于阻塞状态的线程
+*/
+func (u *JustOnceSameTime) Release(RequestTag interface{}) {
 	u.Lock.Lock()
-	close(u.Map[str])
-	delete(u.Map,str)
+	if _, ok := u.Map[RequestTag]; !ok {
+		log.Println("锁已释放？还是不存在？RequestTag用错？RequestTag: ", RequestTag)
+		u.Lock.Unlock()
+		return
+	}
+	close(u.Map[RequestTag])
+	delete(u.Map, RequestTag)
 	u.Lock.Unlock()
-	//log.Println("释放锁")
+	log.Println("释放锁:", RequestTag)
 }
-
-
